b241c0c74fa40a7e85f0c46ab601a227b11f89b9
Yiting Shen <shenyiting2018@gmail.com>
2022-08-07 14:36:59 -0700

    add apis

--- seckill/data_access.py
+++ seckill/data_access.py
@@ -1,5 +1,6 @@
-from .models import Commodity
 from typing import List
+from datetime import datetime
+from .models import Commodity, SeckillCampaign
 
 
 def load_commodity_by_id(commodity_id: int) -> Commodity:
@@ -35,6 +36,7 @@ def create_commodity(
         commodity_price=price,
     )
     new_commodity.save()
+    return new_commodity.id
 
 
 def update_commodity(
@@ -54,3 +56,92 @@ def update_commodity(
         if commodity_description:
             commodity.commodity_description = commodity_description
         commodity.save()
+
+
+def load_seckill_campaign_by_id(seckill_campaign_id: int) -> SeckillCampaign:
+    seckill_campaign = SeckillCampaign.objects.get(id=seckill_campaign_id)
+    return seckill_campaign
+
+
+def list_seckill_campaigns_by_status(campaign_status) -> List[SeckillCampaign]:
+    if campaign_status not in (0, 1, 2):
+        raise Exception("Invalid status!")
+    campaign_list = SeckillCampaign.objects.filter(campaign_status=campaign_status)
+    return list(campaign_list)
+
+
+def create_seckill_campaign(
+    campaign_name,
+    commodity,
+    original_price=0,
+    seckill_price=0,
+    start_time=None,
+    end_time=None,
+    total_stock=0,
+    available_stock=0,
+) -> SeckillCampaign:
+    # commodity = load_commodity_by_id(commodity_id)
+    # if start_time:
+    #     start_time = datetime.strptime(start_time.replace(" ", "-"), "%Y-%m-%d-%H:%M:%S")
+    # if end_time:
+    #     end_time = datetime.strptime(end_time.replace(" ", "-"), "%Y-%m-%d-%H:%M:%S")
+    # total_stock = stock
+    # available_stock = stock
+
+    new_campaign = SeckillCampaign(
+        campaign_name=campaign_name,
+        commodity=commodity,
+        original_price=original_price,
+        seckill_price=seckill_price,
+        campaign_status=2,
+        start_time=start_time,
+        end_time=end_time,
+        total_stock=total_stock,
+        available_stock=available_stock,
+        lock_stock=0,
+    )
+    new_campaign.save()
+    return new_campaign.id
+
+
+def update_seckill_campaign(
+    seckill_campaign_id,
+    campaign_name=None,
+    original_price=None,
+    seckill_price=None,
+    campaign_status=None,
+    start_time=None,
+    end_time=None,
+    total_stock=None,
+    available_stock=None,
+    lock_stock=None,
+):
+    seckill_campaign = load_seckill_campaign_by_id(seckill_campaign_id)
+
+    if campaign_name:
+        seckill_campaign.campaign_name = campaign_name
+    if original_price:
+        seckill_campaign.original_price = original_price
+    if seckill_price:
+        seckill_campaign.seckill_price = seckill_price
+    if campaign_status:
+        if campaign_status not in (0, 1, 2):
+            raise Exception("invalid stauts!")
+        else:
+            seckill_campaign.campaign_status = campaign_status
+    if start_time:
+        start_time = datetime.strptime(
+            start_time.replace(" ", "-"), "%Y-%m-%d-%H:%M:%S"
+        )
+        seckill_campaign.start_time = start_time
+    if end_time:
+        end_time = datetime.strptime(end_time.replace(" ", "-"), "%Y-%m-%d-%H:%M:%S")
+        seckill_campaign.end_time = end_time
+    if total_stock:
+        seckill_campaign.total_stock = total_stock
+    if available_stock:
+        seckill_campaign.available_stock = available_stock
+    if lock_stock:
+        seckill_campaign.lock_stock = lock_stock
+
+    seckill_campaign.save()
--- seckill/views.py
+++ seckill/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.forms.models import model_to_dict
+from seckill.logic import (
+    add_seckill_campaign,
+    available_seckill_campaigns_list,
+    seckill_campaign_item_info,
+)
 
-# Create your views here.
+
+def seckill_campaigns_list(request):
+    campaigns_list = available_seckill_campaigns_list()
+    res = dict()
+    res["campaigns_list"] = campaigns_list
+    return HttpResponse([model_to_dict(campaign) for campaign in campaigns_list])
+
+
+def seckill_campaign_view(request):
+    print("hello")
+    if request.method == "GET":
+        # import ipdb; ipdb.set_trace()
+        campaign_id = request.GET.get("id", None)
+        campaign = seckill_campaign_item_info(campaign_id)
+        return HttpResponse(model_to_dict(campaign))
+    else:
+        campaign_name = request.POST.get("campaign_name", None)
+        commodity_id = request.POST.get("commodity_id", None)
+        original_price = request.POST.get("original_price", None)
+        seckill_price = request.POST.get("seckill_price", None)
+        start_time = request.POST.get("start_time", None)
+        end_time = request.POST.get("end_time", None)
+        stock = request.POST.get("stock", None)
+
+        new_campaign = add_seckill_campaign(
+            campaign_name,
+            commodity_id,
+            original_price,
+            seckill_price,
+            start_time,
+            end_time,
+            stock,
+        )
+
+        return HttpResponse("Success! The campaign id is %s" % (new_campaign.id))
